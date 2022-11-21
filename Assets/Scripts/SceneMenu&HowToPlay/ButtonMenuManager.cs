c8229bbeb212a63239ef0288068136097460c619
jiratQ <jirat123@icloud.com>
2022-11-21 21:29:04 +0700

    add howtoplay

---
+++ Assets/Scripts/SceneMenu&HowToPlay/ButtonMenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenuManager : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Credit()
+    {
+        Application.OpenURL("https://github.com/JiMeow/OreClicker");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
